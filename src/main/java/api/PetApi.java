fb5a84871558bea5cdde15a1006fe538eacb1a2c
OleksandrKupchuk <55634677+OleksandrKupchuk@users.noreply.github.com>
2024-06-08 14:34:42 +0300

    delete logger

--- src/main/java/api/PetApi.java
+++ src/main/java/api/PetApi.java
@@ -3,13 +3,9 @@ package api;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import model.Pet;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class PetApi {
-    private static final Logger logger = LogManager.getLogger(PetApi.class);
     public Response addPet(Pet body){
-        logger.info("---GET PET RESPONSE---");
         Response response = RestAssured.given()
                 .body(body)
                 .post("/pet");
@@ -17,14 +13,12 @@ public class PetApi {
     }
 
     public Response getPetById(int id){
-        logger.info("---GET PET BY ID RESPONSE---");
         Response response = RestAssured.given()
                 .get("/pet/{id}", id);
         return response;
     }
 
     public void updatePet(int id, String name, String status){
-        logger.info("---UPDATE QUERY PET RESPONSE---");
         Response response = RestAssured.given()
                 .pathParams("petId", id)
                 .queryParam("name", name)
@@ -34,7 +28,6 @@ public class PetApi {
     }
 
     public Response updatePet(Pet body){
-        logger.info("---UPDATE BODY PET RESPONSE---");
         Response response = RestAssured.given()
                 .body(body)
                 .put("/pet");
@@ -43,7 +36,6 @@ public class PetApi {
     }
 
     public Response deletePet(int id){
-        logger.info("---DELETE PET RESPONSE---");
         Response response = RestAssured.given()
                 .delete("/pet/{id}", id);
         return response;
